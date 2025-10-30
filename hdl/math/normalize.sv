ab08575035fb53b3ca75363497f284ee7ee7c4dc
William Y. Feng <womogenes2@gmail.com>
2025-10-30 17:36:32 -0400

    Make skeleton for normalize module

---
+++ hdl/math/normalize.sv
@@ -0,0 +1,14 @@
+// Normalize a vec3 into a vec3s
+
+module normalize (
+  input wire clk,
+  input wire rst,
+
+  input vec3 din,
+  input wire din_valid,
+
+  output vec3 dout,
+  output logic dout_valid
+);
+  // TODO: normalize module
+endmodule
