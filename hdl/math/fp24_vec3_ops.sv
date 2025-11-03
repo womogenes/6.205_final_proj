4bd40c915d23e988661a3e70a2f91f46c2eb0566
William Y. Feng <womogenes2@gmail.com>
2025-11-03 17:04:55 -0500

    Make test for vec3 scale

--- hdl/math/fp24_vec3_ops.sv
+++ hdl/math/fp24_vec3_ops.sv
@@ -28,3 +28,18 @@ module fp24_vec3_mul (
   fp24_mul mul_y(.clk(clk), .rst(rst), .a(a.y), .b(b.y), .prod(prod.y));
   fp24_mul mul_z(.clk(clk), .rst(rst), .a(a.z), .b(b.z), .prod(prod.z));
 endmodule
+
+// Multiply a vec3 by a scalar
+module fp24_vec3_scale (
+  input wire clk,
+  input wire rst,
+
+  input fp24_vec3 a,
+  input fp24 s,
+
+  output fp24_vec3 prod
+);
+  fp24_mul mul_x(.clk(clk), .rst(rst), .a(a.x), .b(s), .prod(prod.x));
+  fp24_mul mul_y(.clk(clk), .rst(rst), .a(a.y), .b(s), .prod(prod.y));
+  fp24_mul mul_z(.clk(clk), .rst(rst), .a(a.z), .b(s), .prod(prod.z));
+endmodule
