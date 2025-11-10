260556552ceb86f0ab92f813c907eb7ec52da064
William Y. Feng <womogenes2@gmail.com>
2025-11-09 23:46:53 -0500

    Start implementing parallel rtx testbench

---
+++ hdl/rtx/rtx_tb_parallel.sv
@@ -0,0 +1,106 @@
+`default_nettype none
+
+// Testbench for rtx
+// All this does is wrap rtx but provide scene buffer as well
+
+module rtx_tb #(
+  parameter WIDTH = 1280,
+  parameter HEIGHT = 720
+) (
+  input wire clk,
+  input wire rst,
+  input camera cam,
+
+  input logic [10:0] pixel_h_in,
+  input logic [9:0] pixel_v_in,
+
+  output logic [15:0] rtx_pixel,
+  // output logic [10:0] pixel_h_out,
+  // output logic [9:0] pixel_v_out,
+  output logic ray_done          // i.e. pixel_color valid
+);
+  logic [$clog2(SCENE_BUFFER_DEPTH)-1:0] obj_idx;
+  object obj;
+  logic obj_last;
+
+  logic [10:0] pixel_h_caster;
+  logic [9:0] pixel_v_caster;
+
+  fp24_vec3 ray_origin, ray_dir;
+  logic ray_valid_caster;
+
+  // Initialize scene buffer
+  // Bind inputs to ray tracer
+  scene_buffer #(.INIT_FILE("scene_buffer.mem")) scene_buf (
+    .clk(clk),
+    .rst(rst),
+    .obj_idx(obj_idx),
+    .obj(obj),
+    .obj_last(obj_last)
+  );
+
+  ray_maker #(
+    .WIDTH(WIDTH),
+    .HEIGHT(HEIGHT)
+  ) maker (
+    .clk(clk),
+    .rst(rst),
+
+    // Inputs
+    .cam(cam),
+    .pixel_h_in(pixel_h_in),
+    .pixel_v_in(pixel_v_in),
+    .ray_origin(ray_origin),
+    .ray_dir(ray_dir),
+    .ray_valid(ray_valid_caster),
+
+    // Outputs
+    .pixel_h_out(pixel_h_caster),
+    .pixel_v_out(pixel_v_caster)
+  );
+
+  logic ray_done_tracer;
+  fp24_color pixel_color;
+
+  ray_tracer #(
+    .WIDTH(WIDTH),
+    .HEIGHT(HEIGHT)
+  ) tracer (
+    .clk(clk),
+    .rst(rst),
+
+    // Input
+    .pixel_h_in(pixel_h_caster),
+    .pixel_v_in(pixel_v_caster),
+    .ray_origin(ray_origin),
+    .ray_dir(ray_dir),
+    .ray_valid(1'b1),
+
+    // Doubles as a "pixel valid" signal
+    .ray_done(ray_done_tracer),
+    .pixel_color(pixel_color),
+    // .pixel_h_out(pixel_h),
+    // .pixel_v_out(pixel_v),
+
+    // Scene buffer interface
+    .obj_idx(obj_idx),
+    .obj(obj),
+    .obj_last(obj_last)
+  );
+
+  // Convert to 565 representation
+  fp24_color pixel_color_clipped;
+  fp24_clip_upper #(.UPPER_BOUND(24'h3f0000)) r_min(.clk(clk), .a(pixel_color.r), .clipped(pixel_color_clipped.r));
+  fp24_clip_upper #(.UPPER_BOUND(24'h3f0000)) g_min(.clk(clk), .a(pixel_color.g), .clipped(pixel_color_clipped.g));
+  fp24_clip_upper #(.UPPER_BOUND(24'h3f0000)) b_min(.clk(clk), .a(pixel_color.b), .clipped(pixel_color_clipped.b));
+
+  convert_fp24_uint #(.WIDTH(5), .FRAC(5)) r_convert (.clk(clk), .x(pixel_color_clipped.r), .n(rtx_pixel[4:0]));
+  convert_fp24_uint #(.WIDTH(6), .FRAC(6)) g_convert (.clk(clk), .x(pixel_color_clipped.g), .n(rtx_pixel[10:5]));
+  convert_fp24_uint #(.WIDTH(5), .FRAC(5)) b_convert (.clk(clk), .x(pixel_color_clipped.b), .n(rtx_pixel[15:11]));
+
+  // Delay ray_done by 1 cycle for the conversion
+  pipeline #(.WIDTH(1), .DEPTH(2)) ray_done_pipe (.clk(clk), .in(ray_done_tracer), .out(ray_done));
+
+endmodule
+
+`default_nettype wire
---
+++ sim/rtx/test_rtx_parallel.py
@@ -0,0 +1,144 @@
+import os
+import sys
+import shutil
+
+from pathlib import Path
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import Timer, ClockCycles, RisingEdge, FallingEdge, ReadOnly
+from cocotb.runner import get_runner
+
+from enum import Enum
+import random
+import ctypes
+import numpy as np
+
+from PIL import Image
+from tqdm import tqdm
+
+sys.path.append(Path(__file__).resolve().parent.parent._str)
+from utils import convert_fp24, make_fp24, convert_fp24_vec3, pack_bits, make_fp24_vec3
+
+scale = 0.5
+WIDTH = int(32 * scale)
+HEIGHT = int(18 * scale)
+
+test_file = os.path.basename(__file__).replace(".py", "")
+
+@cocotb.test()
+async def test_module(dut):
+    """cocotb test for the lazy mult module"""
+    dut._log.info("Starting...")
+    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+
+    dut._log.info("Holding reset...")
+    dut.rst.value = 1
+    await ClockCycles(dut.clk, 100)
+    dut.rst.value = 0
+
+    dut.cam.value = pack_bits([
+        (make_fp24_vec3((0, 0, 0)), 72),            # origin
+        (make_fp24_vec3((0, 0, WIDTH / 2)), 72),    # forward
+        (make_fp24_vec3((1, 0, 0)), 72),            # right
+        (make_fp24_vec3((0, 1, 0)), 72),            # up
+    ])
+
+    img = Image.new("RGB", (WIDTH, HEIGHT))
+
+    def unpack_color8(color8):
+        return (
+            ((color8 >> 0) & 0b11111) << 3,
+            ((color8 >> 5) & 0b111111) << 2,
+            ((color8 >> 11) & 0b11111) << 3
+        )
+
+    for i in tqdm(range(WIDTH * HEIGHT), ncols=80, gui=False):
+        pixel_v_in = i // WIDTH
+        pixel_h_in = i % WIDTH
+
+        dut.pixel_h_in.value = pixel_h_in
+        dut.pixel_v_in.value = pixel_v_in
+
+        await RisingEdge(dut.ray_done)
+
+        pixel_color = unpack_color8(dut.rtx_pixel.value.integer)
+
+        r, g, b = pixel_color
+        img.putpixel((pixel_h_in, pixel_v_in), (r, g, b))
+
+    img.save("test.png")
+
+
+def runner():
+    """Module tester."""
+
+    hdl_toplevel_lang = os.getenv("HDL_TOPLEVEL_LANG", "verilog")
+    sim = os.getenv("SIM", "icarus")
+    proj_path = Path(__file__).resolve().parent.parent.parent
+    sys.path.append(str(proj_path / "sim" / "model"))
+    sources = [
+        proj_path / "hdl" / "pipeline.sv",
+        proj_path / "hdl" / "constants.sv",
+        proj_path / "hdl" / "types" / "types.sv",
+        proj_path / "hdl" / "math" / "clz.sv",
+        proj_path / "hdl" / "math" / "fp24_shift.sv",
+        proj_path / "hdl" / "math" / "fp24_add.sv",
+        proj_path / "hdl" / "math" / "fp24_clip.sv",
+        proj_path / "hdl" / "math" / "fp24_mul.sv",
+        proj_path / "hdl" / "math" / "fp24_inv_sqrt.sv",
+        proj_path / "hdl" / "math" / "fp24_sqrt.sv",
+        proj_path / "hdl" / "math" / "fp24_vec3_ops.sv",
+        proj_path / "hdl" / "math" / "fp24_convert.sv",
+        proj_path / "hdl" / "rng" / "prng_sphere.sv",
+        proj_path / "hdl" / "math" / "quadratic_solver.sv",
+        proj_path / "hdl" / "math" / "sphere_intersector.sv",
+        proj_path / "hdl" / "rtx" / "ray_signal_gen.sv",
+        proj_path / "hdl" / "rtx" / "ray_maker.sv",
+        proj_path / "hdl" / "rtx" / "ray_caster.sv",
+
+        proj_path / "hdl" / "mem" / "xilinx_true_dual_port_read_first_2_clock_ram.v",
+        proj_path / "hdl" / "rtx" / "scene_buffer.sv",
+
+        proj_path / "hdl" / "rtx" / "ray_intersector.sv",
+        proj_path / "hdl" / "rtx" / "ray_reflector.sv",
+        proj_path / "hdl" / "rtx" / "ray_tracer.sv",
+        proj_path / "hdl" / "rtx" / "rtx.sv",
+        proj_path / "hdl" / "rtx" / "rtx_tb.sv",
+    ]
+    build_test_args = ["-Wall"]
+
+    build_dir = proj_path / "sim" / "sim_build"
+    shutil.copy(str(proj_path / "data" / "scene_buffer.mem"), build_dir / "scene_buffer.mem")
+
+    # values for parameters defined earlier in the code.
+    parameters = {
+        "WIDTH": WIDTH,
+        "HEIGHT": HEIGHT,
+    }
+
+    sys.path.append(str(proj_path / "sim"))
+    hdl_toplevel = "rtx_tb"
+    
+    runner = get_runner(sim)
+    runner.build(
+        sources=sources,
+        hdl_toplevel=hdl_toplevel,
+        always=True,
+        build_args=build_test_args,
+        parameters=parameters,
+        timescale=("1ns", "1ps"),
+        waves=True,
+        build_dir=build_dir,
+    )
+    run_test_args = []
+    runner.test(
+        hdl_toplevel=hdl_toplevel,
+        test_module=test_file,
+        test_args=run_test_args,
+        waves=True,
+    )
+
+
+if __name__ == "__main__":
+    runner()
