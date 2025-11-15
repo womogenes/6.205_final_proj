34ca80830150f339e125e2a05c12390fad0e7f15
William Y. Feng <womogenes2@gmail.com>
2025-11-14 21:10:33 -0500

    Set up scene with shiny ball in the center

--- sw_raytrace/scene_buffer.c
+++ sw_raytrace/scene_buffer.c
@@ -11,12 +11,12 @@ const Object SCENE_BUFFER[] = {
     .mat = (Material){
       .color = {1, 0.3, 0.3},
       .spec_color = {1.0, 1.0, 1.0},
-      .emit_color = {0.1, 0.1, 0.1},
-      .smoothness = 1.0f,
-      .specular_prob = 0.9f,
+      .emit_color = {0.0, 0.0, 0.0},
+      .smoothness = 0.5f,
+      .specular_prob = 1.0f,
     },
-    .sphere_center = {-2, 1, 7},
-    .sphere_rad_sq = 2 * 2,
+    .sphere_center = {-2, 0, 4},
+    .sphere_rad_sq = 1 * 1,
   },
   // Green sphere
   (Object){
@@ -24,11 +24,11 @@ const Object SCENE_BUFFER[] = {
     .mat = (Material){
       .color = {0.3, 1, 0.3},
       .spec_color = {1.0, 1.0, 1.0},
-      .emit_color = {0.1, 0.1, 0.1},
+      .emit_color = {0.0, 0.0, 0.0},
       .smoothness = 1.0f,
-      .specular_prob = 0.9f,
+      .specular_prob = 1.0f,
     },
-    .sphere_center = {0, 0, 5},
+    .sphere_center = {0, 0, 4},
     .sphere_rad_sq = 1 * 1,
   },
   // Blue sphere
@@ -37,12 +37,25 @@ const Object SCENE_BUFFER[] = {
     .mat = (Material){
       .color = {0.3, 0.3, 1},
       .spec_color = {1.0, 1.0, 1.0},
-      .emit_color = {0.1, 0.1, 0.1},
+      .emit_color = {0.0, 0.0, 0.0},
       .smoothness = 0.0f,
       .specular_prob = 0.0f,
     },
-    .sphere_center = {1.0, -0.5, 4},
-    .sphere_rad_sq = 0.5 * 0.5,
+    .sphere_center = {2, 0, 4},
+    .sphere_rad_sq = 1 * 1,
+  },
+  // Green sphere, behind
+  (Object){
+    .is_trig = 0,
+    .mat = (Material){
+      .color = {0.3, 1, 0.3},
+      .spec_color = {1.0, 1.0, 1.0},
+      .emit_color = {0.0, 0.0, 0.0},
+      .smoothness = 0.0f,
+      .specular_prob = 0.0f,
+    },
+    .sphere_center = {0, 0, 0.9},
+    .sphere_rad_sq = 1 * 1,
   },
   // Skylight
   (Object){
@@ -55,7 +68,7 @@ const Object SCENE_BUFFER[] = {
       .specular_prob = 0.0f,
     },
     .sphere_center = {0, 500, 0},
-    .sphere_rad_sq = 450 * 450,
+    .sphere_rad_sq = 200 * 200,
   },
   // "Ground"
   (Object){
@@ -67,8 +80,8 @@ const Object SCENE_BUFFER[] = {
       .smoothness = 1.0f,
       .specular_prob = 0.1f,
     },
-    .sphere_center = {0, -200, 5},
-    .sphere_rad_sq = 199 * 199,
+    .sphere_center = {0, -2000, 5},
+    .sphere_rad_sq = 1999 * 1999,
   },
 };
 
